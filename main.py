6999e87b506e1c6df8f7bf9b62524efeaaf7a72c
Emil Karlström <emkl@WIN10-EMILK.localdomain>
2023-01-05 16:55:24 +0100

    totally didn't steal code and rewrite the names of variables to pretend it is ours, fuck you

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from deepspeech_pytorch.configs.inference_config import TranscribeConfig
 import torchaudio
 from attacker import Attacker
 model_path = '/home/emkl/Documents/School/DV2607/ds2aa/models/librispeech/librispeech_pretrained_v3.ckpt'
-audio_path = '/home/emkl/Documents/School/DV2607/ds2aa/audio16k/harvard.wav'
+audio_path = '/home/emkl/Documents/School/DV2607/ds2aa/audio16k/osr34.wav'
 
 target_sentence_str = 'I AM A BIG FAN OF COOKIES AND I WOULD LIKE IF YOU GAVE ME SOME ORANGES'
 
---
+++ preprocess_audio.py
@@ -0,0 +1,15 @@
+import librosa    
+import os
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_file', type=str, help='Original wav file', dest='input_file')
+    parser.add_argument('--output_file', type=str, help='Output wav file', dest='output_file')
+    args = parser.parse_args()
+
+    wav_file = args.input_file
+    if wav_file[-4:] == ".wav":
+        print(f"Convert {wav_file}")
+        y, s = librosa.load(wav_file, sr=16000)
+        librosa.output.write_wav(args.output_file, y, s)
